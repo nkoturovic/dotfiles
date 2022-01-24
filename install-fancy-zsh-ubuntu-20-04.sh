a1662c5e74b68ec02d1ad1dbb040102cefab2db8
Nebojša Koturović <nebojsa.koturovic@united.cloud>
2022-01-24 17:21:08 +0100

    Edit success message in zsh install script

--- install-fancy-zsh-ubuntu-20-04.sh
+++ install-fancy-zsh-ubuntu-20-04.sh
@@ -59,11 +59,11 @@ chmod +rx ~/.p10k.zsh
 echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
 rm -r $HOME/fancy-zsh-installer
 printf "
-===========================================
+=============================================================
 You have successfully installed fancy zsh\n
-===========================================
-Open your Terminal emulator app, and set:
-- shell to zsh
-- font to: MesloLGS NF
-===========================================
+=============================================================
+Open/Restart your Terminal, in terminal settings/profile set:
+- shell to: zsh
+- font to : MesloLGS NF
+=============================================================
 "
